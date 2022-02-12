9fb63bd04a149a41b535482323d3376a9d6a3ac5
Abhijeetrath2021 <98682157+Abhijeetrath2021@users.noreply.github.com>
2022-02-12 18:41:24 +0530

    Abhijeet_assignment_1

---
+++ week_1_ques4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char a[50];int e;char b[50];char c[100];
+    printf("Name of the student:\n");
+    gets(a);
+    printf("Branch:\n");
+    gets(b);
+    printf("My Hobbies are\n");
+    gets(c);
+    printf("Regd No.:\n");
+    scanf("%d",&e);
+    printf("Name:%s\n Branch:%s\n Hobbies:%s\n", &a, &b, &c);
+    return 0;
+}
